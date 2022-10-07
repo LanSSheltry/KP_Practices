5f77e0d5f5e03dbde6eef5fcb18c7655f38e077c
Vladimir Aldokhin <vladimiraldokhin05@gmail.com>
2022-10-07 10:29:29 +0200

    Repository init and test

---
+++ Practice_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+namespace Practice_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Practice no.1\nVolodymyr Aldokhin\nIPZ-41\n\n");
+            ReadKey();
+        }
+    }
+}
